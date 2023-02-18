2583455f73db0ff870df74292bbf9e6a8045e370
zwj <2309840734@qq.com>
2023-02-19 00:14:52 +0800

    fix(package.json): 修改package.json 脚本配置

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -2,6 +2,6 @@
 // let baseURL = 'http://123.207.32.32:8000/home/multidata';
 
 // 线上地址
-let baseURL = import.meta.env.VITE_APP_BASE_URL;
+const baseURL = import.meta.env.VITE_APP_BASE_URL;
 
 export default baseURL;
