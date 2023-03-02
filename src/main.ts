5f89bb3e7c8c2b3ac59da45413d43fc777d2ff49
zwj <2309840734@qq.com>
2023-03-02 11:24:36 +0800

    fix(main.ts): 解决reset.css文件引入无效问题

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import 'normalize.css';
-
+import './assets/css/reset.css';
 import router from '@/router/index';
 import pinia from '@/store/index';
 
